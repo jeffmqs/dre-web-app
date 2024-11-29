116075bc2166fe17456cc0529f827290f6d459fc
Cauã Sebastian <133614515+Cauasebastian@users.noreply.github.com>
2024-11-29 15:41:43 -0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/auth': {
+        target: 'https://dre-backend-49rf.onrender.com', // URL do backend
+        changeOrigin: true, // Ajusta a origem para corresponder ao backend
+        secure: false, // Caso o backend use HTTPS com certificado autoassinado
+      },
+      '/api': {
+        target: 'https://dre-backend-49rf.onrender.com', // URL do backend
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
